69daea3c11d16acf6e0d4408d00d06ceb8ec14ce
shame-game <hoanghung.vam1209@gmail.com>
2024-02-06 20:11:19 +0700

    d

--- Js/index.js
+++ Js/index.js
@@ -5,18 +5,18 @@ window.onload = () => {
     vam('.loadweb').remove();
 };
 
-vam('.openmenu').addEventListener('click',()=>{
-    vam('.header1412a2-nav-mb-wrap').setAttribute('style','transform: translateX(0)')
-    vam('.bg').setAttribute('style','display:block')
+vam('.openmenu').addEventListener('click', () => {
+    vam('.header1412a2-nav-mb-wrap').setAttribute('style', 'transform: translateX(0)')
+    vam('.bg').setAttribute('style', 'display:block')
 })
 
-vam('.clostmenu').addEventListener('click',()=>{
-    vam('.header1412a2-nav-mb-wrap').setAttribute('style','transform: translateX(100%)')
-    vam('.bg').setAttribute('style','display:none')
+vam('.clostmenu').addEventListener('click', () => {
+    vam('.header1412a2-nav-mb-wrap').setAttribute('style', 'transform: translateX(100%)')
+    vam('.bg').setAttribute('style', 'display:none')
 })
-vam('.bg').addEventListener('click',()=>{
-    vam('.header1412a2-nav-mb-wrap').setAttribute('style','transform: translateX(100%)')
-    vam('.bg').setAttribute('style','display:none')
+vam('.bg').addEventListener('click', () => {
+    vam('.header1412a2-nav-mb-wrap').setAttribute('style', 'transform: translateX(100%)')
+    vam('.bg').setAttribute('style', 'display:none')
 })
 
 vam('#c661412b2-iframe_click').addEventListener('click', () => {
@@ -35,13 +35,26 @@ vam('#c661412b2-iframe_click').addEventListener('click', () => {
 vam('#c661412b2-iframe_click').addEventListener('click', () => {
     vam('.c661412b2-background_click').setAttribute('style', 'display:flex')
     vam('.c661412b2-iframe_click').setAttribute('style', 'display:flex')
-    var c661412b2Iframe = `<iframe  class="c661412b2-iframe" src="https://www.youtube.com/embed/2rq6Vph2Sfc?si=xLhapCgHCTZxgnuT" frameborder="0"
+    var c661412b2Iframe = `<iframe  class="c661412b2-iframe" src="https://www.youtube.com/embed/Lx-E2F1ejCA?si=I-kSBhKVM2oUl_o3" frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`
     vam('.c661412b2-iframe_click').innerHTML = c661412b2Iframe
     vam('.c661412b2-background_click').addEventListener('click', () => {
         vam('.c661412b2-background_click').setAttribute('style', 'display:none')
         vam('.c661412b2-iframe_click').setAttribute('style', 'display:none')
         vam('.c661412b2-iframe').remove()
+    })
+})
+
+vam('.c661412b1button-click').addEventListener('click', () => {
+    vam('.c661412b1-background_click').setAttribute('style', 'display:flex')
+    vam('.c661412b1-iframe_click').setAttribute('style', 'display:flex')
+    var c661412b1Iframe = `<iframe  class="c661412b1-iframe" src="https://www.youtube.com/embed/Lx-E2F1ejCA?si=I-kSBhKVM2oUl_o3" frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`
+    vam('.c661412b1-iframe_click').innerHTML = c661412b1Iframe
+    vam('.c661412b1-background_click').addEventListener('click', () => {
+        vam('.c661412b1-background_click').setAttribute('style', 'display:none')
+        vam('.c661412b1-iframe_click').setAttribute('style', 'display:none')
+        vam('.c661412b1-iframe').remove()
     })
 })
 
@@ -58,14 +71,47 @@ vam("#formpay").addEventListener("submit", event => {
     formData.append('entry.1107779425', document.querySelector("input[name='entry.1107779425']").value);
     formData.append('entry.949156637', document.querySelector("input[name='entry.949156637']").value);
     fetch(URL + "?" + formData.toString(), {
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-      },
-      method: "post",
-      "mode": "cors"
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+        },
+        method: "post",
+        "mode": "cors"
     });
 
     alert("Gửi thông tin thành công");
     document.querySelector('.thongtin .nextt').removeAttribute('disabled')
-  });
+});
+
+vam("#dkform").addEventListener("submit", event => {
+    // prevent submit
+    event.preventDefault();
+
+    // call as API
+    const URL = "https://docs.google.com/forms/d/1yPiTQ5wou6Y3VxV5_Bex9C7bRVVIUvY_Jp7RaktKgyg/formResponse";
+
+    let formData = new URLSearchParams();
+    formData.append('entry.214186861', document.querySelector("input[name='entry.214186861']").value);
+    formData.append('entry.1144448489', document.querySelector("input[name='entry.1144448489']").value);
+    formData.append('entry.178791406', document.querySelector("input[name='entry.178791406']").value);
+    formData.append('entry.871869972', document.querySelector("input[name='entry.871869972']").value);
+    fetch(URL + "?" + formData.toString(), {
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+        },
+        method: "post",
+        "mode": "cors"
+    });
+
+    vam('.tbsuc').setAttribute('style', 'transform:translateX(0)')
+    vam("#dkform").reset();
+    setTimeout(()=>{
+        vam('.tbsuc').setAttribute('style', 'transform:translateX(200%)')
+    }, 3000);
+
+});
+
+vam('.bi-x').addEventListener('click', () => {
+    vam('.tbsuc').setAttribute('style', 'transform:translateX(200%)')
+})
